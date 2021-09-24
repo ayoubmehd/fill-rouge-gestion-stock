30c853d4c3ea7ab6fbed4e17282ccc1446cc3c7c
ayoubmehd <safi-_-gio-raja@live.fr>
2021-09-24 23:38:17 +0100

    product and categorie factory

---
+++ database/factories/ProduitFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Produit;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProduitFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Produit::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->sentence(),
+            'prix' => $this->faker->randomFloat(2),
+            'description' => $this->faker->paragraphs(5),
+            'quantite' => $this->faker->randomNumber(3)
+        ];
+    }
+}
