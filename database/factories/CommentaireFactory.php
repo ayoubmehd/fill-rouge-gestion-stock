f86b3a59c6c9bc7ea29f8431acbb2fc6656b374a
ayoubmehd <safi-_-gio-raja@live.fr>
2021-09-24 23:41:03 +0100

    create comment factory

---
+++ database/factories/CommentaireFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Commentaire;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CommentaireFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Commentaire::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'contenu' => $this->faker->paragraph(1),
+            'point' => $this->faker->randomElement([-1, 0, 1])
+        ];
+    }
+}
