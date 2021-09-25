7b083b50ffdef116496cca759ab8624db3e564eb
ayoubmehd <safi-_-gio-raja@live.fr>
2021-09-25 01:16:10 +0100

    chore: create produit categorie relation

--- app/Models/Categorie.php
+++ app/Models/Categorie.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Categorie extends Model
 {
     use HasFactory;
+
+    public function produits()
+    {
+        return $this->hasMany(Produit::class);
+    }
 }
