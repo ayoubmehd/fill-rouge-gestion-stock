07d3000db4053fea3b977da294af709476b2d7a1
ayoubmehd <safi-_-gio-raja@live.fr>
2021-09-25 01:06:53 +0100

    fix: categorie_id should be on produits table not the opposite

--- database/migrations/2021_09_24_225815_fk_categories_produits.php
+++ database/migrations/2021_09_24_225815_fk_categories_produits.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCategoriesTable extends Migration
+class FkCategoriesProduits extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,8 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
-            $table->id();
-            $table->string('lable')->nullable();
-            $table->string('slug')->nullable();
-            $table->timestamps();
+        Schema::table('produits', function (Blueprint $table) {
+            $table->foreignId('categorie_id')->constrained();
         });
     }
 
@@ -28,6 +25,8 @@ class CreateCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::table('produits', function (Blueprint $table) {
+            //
+        });
     }
 }
