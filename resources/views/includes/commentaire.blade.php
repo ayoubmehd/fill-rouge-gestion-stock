64a6d004f3858c15b927722d0bdb30ebee095c52
ayoubmehd <safi-_-gio-raja@live.fr>
2021-10-10 13:24:26 +0100

    create commentaire reuseable view

---
+++ resources/views/includes/commentaire.blade.php
@@ -0,0 +1,35 @@
+<div class="card shadow">
+    <div class="card-body">
+        <div class="row justify-content-between">
+            <div class="col-md-4 d-flex flex-column justify-content-between">
+                <h3 class="card-title"># 34
+                    {{-- {{ $commande->id }} --}}
+                </h3>
+                <p class="card-text"></p>
+            </div>
+            <div class=" col-md-6 d-flex flex-column justify-content-between align-items-end">
+                <p class="card-text">
+                    Shipped
+                    <i class="mdi mdi-truck h3 mb-0 order-icon"></i>
+                </p>
+                <p class="card-text">
+                    20 Produits
+                    {{-- {{ $commande->produits_count }} --}}
+                </p>
+            </div>
+            <div class="col-md-8 d-flex flex-column justify-content-between">
+                <h3 class="card-title">
+                    add
+                    {{-- {{ $commentaire->name }} --}}
+                </h3>
+                <p class="card-text">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni id neque distinctio ipsum, nobis maxime architecto corporis! Facilis, laudantium eveniet.
+                    {{-- {{ $commentaire->content }} --}}
+                </p>
+            </div>
+            <div class="col d-flex flex-column justify-content-between align-items-end">
+                <i class="mdi mdi-star-check text-primary h2"></i>
+            </div>
+        </div>
+    </div>
+</div>
