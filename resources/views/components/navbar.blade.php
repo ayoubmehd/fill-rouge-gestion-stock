2d4f47a0ad27879bb6e551cd52fa11540bca54e7
ayoubmehd <safi-_-gio-raja@live.fr>
2021-10-26 12:25:39 +0100

    style: navbar responsive

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -4,18 +4,20 @@
             <i class="mdi mdi-mustache h3 mb-0"></i>
             {{ $title }}
         </a>
-        <ul class="navbar__nav">
+        <ul class="navbar__nav mx-auto w-75">
             @foreach($menuItems as $item)
             <li class="navbar__item">
                 <a class="navbar__link navbar__link-main" href="{{ $item['to'] }}">
                     <i class="mdi {{ $item['icon'] }} h3 mb-0"></i>
-                    {{ $item['text'] }}
+                    <span class="navbar__link-text">
+                        {{ $item['text'] }}
+                    </span>
                 </a>
             </li>
             @endforeach
         </ul>
 
-        <ul class="navbar__nav">
+        <ul class="navbar__nav w-35">
             @foreach($rightMenuItems as $item)
             <li class="navbar__item">
                 <form action="{{ $item['to'] }}" method="post">
